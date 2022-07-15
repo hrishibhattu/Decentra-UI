74b4157c3471b9c10b85b0e0a87abd32a6c84fc5
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-16 01:14:21 +0200

    Update Headertitle.js

--- components/home/Headertitle.js
+++ components/home/Headertitle.js
@@ -16,22 +16,6 @@ export default function Headertitle() {
         fontFamily: 'Screen',
         borderRadius: '15px',
         gap: '20px',
-
-        '&:hover': {
-          background: '$violet5',
-          color: '$mauve12',
-          border: '1px solid $violet6',
-        },
-        '&:active': {
-          background: '$violet6',
-          color: '$mauve12',
-          border: '1px solid $violet7',
-        },
-
-        '@media (max-width: 768px)': {
-          fontSize: '18px',
-          minWidth: '25px',
-        },
       }}
     >
       We help you create a DAO without code. In less than 5 minutes.
