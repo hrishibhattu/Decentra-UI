872d793e41d3e1b507555f1dbcbed056a6a73235
d3centra <d3centra@users.noreply.github.com>
2022-07-15 16:18:12 +0000

    Applying prettier

--- components/home/Welcome.js
+++ components/home/Welcome.js
@@ -5,8 +5,7 @@ import ActivityLog from './ActivityLog'
 export default function Welcome({ allDaos }) {
   return (
     <Flex dir="row" gap="md">
-      <Text> We help you create a DAO without code. In less than 5 minutes.
-</Text>
+      <Text> We help you create a DAO without code. In less than 5 minutes.</Text>
       <ActivityLog allDaos={allDaos && allDaos} />
       {/* <Featured /> */}
     </Flex>
