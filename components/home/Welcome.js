c563ba1aaca59fc6e85d6d59f20333b70e3363e8
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-16 00:51:53 +0200

    Update Welcome.js

--- components/home/Welcome.js
+++ components/home/Welcome.js
@@ -4,6 +4,9 @@ import ActivityLog from './ActivityLog'
 
 export default function Welcome({ allDaos }) {
   return (
+    <div>
+    We help you create a DAO without code. In less than 5 minutes.
+</div>
     <Flex dir="row" gap="md">
       <ActivityLog allDaos={allDaos && allDaos} />
       {/* <Featured /> */}
