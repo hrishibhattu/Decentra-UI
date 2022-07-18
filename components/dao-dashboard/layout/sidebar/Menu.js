39a26e16df8572beafb6b5ae7ebb173a5a35d44c
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-18 13:37:07 +0200

    Update Menu.js

--- components/dao-dashboard/layout/sidebar/Menu.js
+++ components/dao-dashboard/layout/sidebar/Menu.js
@@ -71,7 +71,7 @@ export default function Menu({ saleActive }) {
             height: '74px',
             borderBottom: '1px solid #a3a3ac1a',
             boxShadow: '0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d',
-            fontSIze: '28px',
+            fontSize: '28px',
           }}
         >
           DAOName
