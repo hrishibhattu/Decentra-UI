8687e8cebc9f26d42003610e7f6539a36d2c1754
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-18 13:23:00 +0200

    Update Menu.js

--- components/dao-dashboard/layout/sidebar/Menu.js
+++ components/dao-dashboard/layout/sidebar/Menu.js
@@ -55,7 +55,7 @@ export default function Menu({ saleActive }) {
     >
       <Link
         href={{
-          pathname: '/daos/[chainId]/[dao]/crowdsale',
+          pathname: '/daos/[chainId]/[dao]',
           query: {
             chainId: router.query.chainId,
             dao: router.query.dao,
@@ -69,7 +69,9 @@ export default function Menu({ saleActive }) {
             display: 'flex',
             alignItems: 'center',
             padding: '0 20px',
-            height: '35px',
+            height: '74px',
+            borderBottom: '1px solid #a3a3ac1a',
+            boxShadow: '0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d',     
           }}
         >
           DAOName
