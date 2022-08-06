4be2c48189a91893b8dad361f936ce32ac772f79
Daniel <95552251+d3centra@users.noreply.github.com>
2022-08-06 17:35:07 +0200

    Update Progress.js

--- styles/Progress.js
+++ styles/Progress.js
@@ -4,14 +4,14 @@ import { styled } from './stitches.config'
 const StyledProgress = styled(ProgressPrimitive.Root, {
   position: 'relative',
   overflow: 'hidden',
-  background: '$violet2',
+  background: 'linear-gradient(45deg, rgba(189, 70, 70, 0.22), rgba(244, 76, 243, 0.22))',
   borderRadius: '3px',
   width: '100%',
   height: 9,
 })
 
 const StyledIndicator = styled(ProgressPrimitive.Indicator, {
-  backgroundColor: '$violet11',
+  background: 'linear-gradient(45deg, rgb(189, 70, 70), rgba(244, 76, 243, 0.69))',
   width: '100%',
   height: '100%',
   transition: 'transform 660ms cubic-bezier(0.65, 0, 0.35, 1)',
